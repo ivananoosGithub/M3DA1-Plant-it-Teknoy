de7e0dd0a1970c95dda9bdb1f0db2e1b1df8dafb
KyTzuki <meadowmc17@gmail.com>
2022-09-03 13:04:24 +0800

    made initial signup for Users

--- team_m3da1_project/Plan_It_Teknoy/urls.py
+++ team_m3da1_project/Plan_It_Teknoy/urls.py
@@ -1,23 +1,22 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
-from . import views
+from Plan_It_Teknoy import views
 
-
-# app_name = 'Plan_It_Teknoy'
+app_name = 'Plan_It_Teknoy'
 
 urlpatterns = [
 
     # Start of user pages
     path('', views.home, name="Home"),
-    path("About", views.about, name="About"),
-    path("Contact", views.contact, name="Contact"),
-    path("Sign in", views.signin, name="Sign in"),
-    path("Sign up", views.signup, name="Sign up"),
-
+    path("About/", views.about, name="About"),
+    path("Contact/", views.contact, name="Contact"),
+    path("SignIn/", views.signin, name="signin"),
+    path("SignUp/", views.SignUpView.as_view(), name="signup_view"),
     # End of user pages
 
     # # icon browser tab
     # path("favicon.ico", RedirectView.as_view(
     #     url=staticfiles_storage.url("favicon.ico"))),
 
-
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- team_m3da1_project/Plan_It_Teknoy/views.py
+++ team_m3da1_project/Plan_It_Teknoy/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-from .forms import*
-
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.views.generic import View
+from django.views.generic import View
+from .forms import *
+from .models import *
+from passlib.hash import pbkdf2_sha256
 
 
 #################################### Start of user pages ###################################
@@ -8,20 +12,58 @@ from .forms import*
 
 # Home and Landing Page
 def home(response):
-    return render(response, "Plan_It_Teknoy/User/Home.html", {})
+    return render(response, "Home.html", {})
 
 # About Us Page
 def about(response):
-    return render(response, "Plan_It_Teknoy/User/About.html", {})
+    return render(response, "About.html", {})
     
 # Contact Us page
 def contact(response):
-    return render(response, "Plan_It_Teknoy/User/Contact.html", {})
+    return render(response, "Contact.html", {})
 
 # Sign in page
 def signin(response):
-    return render(response, "Plan_It_Teknoy/User/signin.html", {})
+    return render(response, "SignIn.html", {})
 
 # Sign up page
-def signup(response):
-    return render(response, "Plan_It_Teknoy/User/signup.html", {})
+class SignUpView(View):
+    def get(self, request):
+        #  if 'user' in request.session:
+        #      current_user = request.session['user']
+        #      users = Users.objects.all()
+        #      context = {
+        #          'current_user': current_user,
+        #          'users' : users,
+        #      }
+        #      return render(request, 'signup.html', context)
+        #  else:
+            return render(request, 'signup.html', {})
+
+    def post(self, request):        
+        form = UsersForm(request.POST, request.FILES)        
+        if form.is_valid():
+            # try:
+            id_number = request.POST.get("id_number")
+            password = request.POST.get("password")
+            confirmpassword = request.POST.get("cpassword")
+            enc_password = pbkdf2_sha256.encrypt(password, rounds=12000, salt_size=32)
+            email = request.POST.get("email")
+            if(confirmpassword == password):
+                if Users.objects.filter(id_number=id_number).count()>0:
+                    messages.info(request, 'ID Number already exists!')
+                    return redirect('Plan_It_Teknoy:signup_view') 
+                else:
+                    form = Users(id_number = id_number, password = enc_password, email = email)
+                    form.save()
+                    check_user = Users.objects.filter(id_number=id_number, password=enc_password, email = email)
+                    if check_user:
+                        request.session['user'] = id_number
+                        return redirect('Plan_It_Teknoy:Home')
+            else:
+                messages.info(request, 'Passwords do not match!', extra_tags='signin')
+                return redirect('Plan_It_Teknoy:signup_view')
+        else:
+            print(form.errors)
+            messages.info(request, 'Account already exists! Please try another unique one.', extra_tags='try')
+            return redirect('Plan_It_Teknoy:signup_view')
