69dbb3952c259202c65d71c6eb3322e3a67e5021
KyTzuki <meadowmc17@gmail.com>
2022-09-07 15:00:46 +0800

    Student and Teacher Sign Up
    Sign Up for both roles now works

--- team_m3da1_project/Plan_It_Teknoy/views.py
+++ team_m3da1_project/Plan_It_Teknoy/views.py
@@ -9,6 +9,13 @@ from passlib.hash import pbkdf2_sha256
 
 #################################### Start of user pages ###################################
 
+# Logout current_user session
+def logout(request):
+    try:
+        del request.session['user']
+    except:
+        return redirect('Plan_It_Teknoy:Home')
+    return redirect('Plan_It_Teknoy:Home')
 
 # Home and Landing Page
 def home(response):
@@ -57,7 +64,21 @@ class contactView(View):
 # Sign in page
 class SignInView(View):
     def get(self, request):
-        return render(request, "signin.html", {})
+        if 'user' in request.session:
+            current_user = request.session['user']
+            students = Students.objects.filter(StudentID=current_user)
+            teachers = Teachers.objects.filter(TeacherID=current_user) 
+            users = Users.objects.filter(id_number=current_user)
+
+            context = {
+                'current_user': current_user,
+                'students' : students,
+                'teachers' : teachers,
+                'users' : users,
+            }
+            return render(request, 'Home.html', context)
+        else:
+            return render(request, 'signin.html', {})
     
     def post(self, request):
         if request.method == 'POST':
@@ -77,49 +98,133 @@ class SignInView(View):
                 return redirect('Plan_It_Teknoy:signin_view') 
         return redirect('Plan_It_Teknoy:Home')
 
-# Sign up page
-class SignUpView(View):
+# Student Sign Up
+class SignUpStudentView(View):
     def get(self, request):
-        #  if 'user' in request.session:
-        #      current_user = request.session['user']
-        #      users = Users.objects.all()
-        #      context = {
-        #          'current_user': current_user,
-        #          'users' : users,
-        #      }
-        #      return render(request, 'signup.html', context)
-        #  else:
-            return render(request, 'signup.html', {})
+        if 'user' in request.session:
+            current_user = request.session['user']
+            students = Students.objects.filter(StudentID=current_user)
+            teachers = Teachers.objects.filter(TeacherID=current_user) 
+            users = Users.objects.filter(id_number=current_user)
+
+            context = {
+                'current_user': current_user,
+                'students' : students,
+                'teachers' : teachers,
+                'users' : users,
+            }
+            return render(request, 'Home.html', context)
+        else:
+            return render(request, 'signup-student.html', {})
 
     def post(self, request):        
-        form = UsersForm(request.POST, request.FILES)        
-        if form.is_valid():
-            # try:
+        form1 = StudentsForm(request.POST, request.FILES)        
+        form2 = UsersForm(request.POST, request.FILES)       
+        if form1.is_valid() or form2.is_valid():
+            # Personal Information
+            first_name = request.POST.get("first_name")
+            middle_name = request.POST.get("middle_name")
+            last_name = request.POST.get("last_name")
+            gender = request.POST.get("gender")
+            department = request.POST.get("department")
+            program = request.POST.get("program")
+            year_level = request.POST.get("year_level")
+            # Contact Information
+            home_address = request.POST.get("home_address")
+            city_address = request.POST.get("city_address")
+            contact_number = request.POST.get("contact_number")
+            # Account Information
             id_number = request.POST.get("id_number")
             password = request.POST.get("password")
             confirmpassword = request.POST.get("cpassword")
             enc_password = pbkdf2_sha256.encrypt(password, rounds=12000, salt_size=32)
             email = request.POST.get("email")
             if(confirmpassword == password):
-                if Users.objects.filter(id_number=id_number).count()>0:
+                if Students.objects.filter(StudentID=id_number).count()>0 or Users.objects.filter(id_number=id_number).count()>0:
                     messages.info(request, 'ID Number already exists!')
-                    return redirect('Plan_It_Teknoy:signup_view') 
-                else:
-                    form = Users(id_number = id_number, password = enc_password, email = email)
-                    form.save()
+                    return redirect('Plan_It_Teknoy:signupS_view') 
+                else:              
+                    form2 = Users(id_number = id_number, password = enc_password, email = email)
+                    form2.save()
+                    
+                    fk_id_number = Users.objects.get(id_number = id_number)
+
+                    form1 = Students(StudentID = fk_id_number, first_name = first_name, middle_name = middle_name, last_name = last_name, gender = gender, department = department, program = program, year_level = year_level, home_address = home_address, city_address = city_address, contact_number = contact_number)
+                    form1.save()
+
                     check_user = Users.objects.filter(id_number=id_number, password=enc_password, email = email)
                     if check_user:
                         request.session['user'] = id_number
                         return redirect('Plan_It_Teknoy:signin_view')
             else:
                 messages.info(request, 'Passwords do not match!', extra_tags='signin')
-                return redirect('Plan_It_Teknoy:signup_view')
+                return redirect('Plan_It_Teknoy:signupS_view')
         else:
-            print(form.errors)
+            print(form1.errors,form2.errors)
             messages.info(request, 'Account already exists! Please try another unique one.', extra_tags='try')
-            return redirect('Plan_It_Teknoy:signup_view')
+            return redirect('Plan_It_Teknoy:signupS_view')
 
-# Select Role Page
-class SignupTeacherView(View):
+# Teacher Sign Up
+class SignUpTeacherView(View):
     def get(self, request):
-        return render(request, 'signup-teacher.html', {})
+        if 'user' in request.session:
+            current_user = request.session['user']
+            students = Students.objects.filter(StudentID=current_user)
+            teachers = Teachers.objects.filter(TeacherID=current_user) 
+            users = Users.objects.filter(id_number=current_user)
+
+            context = {
+                'current_user': current_user,
+                'students' : students,
+                'teachers' : teachers,
+                'users' : users,
+            }
+            return render(request, 'Home.html', context)
+        else:
+            return render(request, 'signup-teacher.html', {})
+
+    def post(self, request):        
+        form1 = StudentsForm(request.POST, request.FILES)        
+        form2 = UsersForm(request.POST, request.FILES)          
+        if form1.is_valid() or form2.is_valid():
+            # Personal Information
+            first_name = request.POST.get("first_name")
+            middle_name = request.POST.get("middle_name")
+            last_name = request.POST.get("last_name")
+            gender = request.POST.get("gender")
+            department = request.POST.get("department")
+            program = request.POST.get("program")
+            # Contact Information
+            home_address = request.POST.get("home_address")
+            city_address = request.POST.get("city_address")
+            contact_number = request.POST.get("contact_number")
+            # Account Information
+            id_number = request.POST.get("id_number")
+            password = request.POST.get("password")
+            confirmpassword = request.POST.get("cpassword")
+            enc_password = pbkdf2_sha256.encrypt(password, rounds=12000, salt_size=32)
+            email = request.POST.get("email")
+            if(confirmpassword == password):
+                if Teachers.objects.filter(TeacherID=id_number).count()>0 or Users.objects.filter(id_number=id_number).count()>0:
+                    messages.info(request, 'ID Number already exists!')
+                    return redirect('Plan_It_Teknoy:signupT_view') 
+                else:
+                    form2 = Users(id_number = id_number, password = enc_password, email = email)
+                    form2.save()
+
+                    fk_id_number = Users.objects.get(id_number = id_number)
+
+                    form1 = Teachers(TeacherID = fk_id_number, first_name = first_name, middle_name = middle_name, last_name = last_name, gender = gender, department = department, program = program, home_address = home_address, city_address = city_address, contact_number = contact_number)
+                    form1.save()
+
+                    check_user = Users.objects.filter(id_number=id_number, password=enc_password, email = email)
+                    if check_user:
+                        request.session['user'] = id_number
+                        return redirect('Plan_It_Teknoy:signin_view')
+            else:
+                messages.info(request, 'Passwords do not match!', extra_tags='signin')
+                return redirect('Plan_It_Teknoy:signupT_view')
+        else:
+            print(form1.errors,form2.errors)
+            messages.info(request, 'Account already exists! Please try another unique one.', extra_tags='try')
+            return redirect('Plan_It_Teknoy:signupT_view')
