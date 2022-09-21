780e1947677e29029914d2c0cb6324570e04f65b
Ivan Kim Anoos <55166879+ivananoosGithub@users.noreply.github.com>
2022-09-21 20:38:02 +0800

    forgot password feature added

--- team_m3da1_project/team_m3da1_project/urls.py
+++ team_m3da1_project/team_m3da1_project/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', include('Plan_It_Teknoy.urls', namespace='Plan_It_Teknoy')), 
