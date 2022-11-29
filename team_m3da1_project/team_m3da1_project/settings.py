e704c388f99ef6a3572e7febfc9bc1b21f779410
McWayne Otsuki <meadowmc17@gmail.com>
2022-11-29 21:05:04 +0800

    .

--- team_m3da1_project/team_m3da1_project/settings.py
+++ team_m3da1_project/team_m3da1_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$z1*08ztw2+hdn*3=amlt20*p216f#3r=f!fve$331q9cm*++c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','https://m3da1.azurewebsites.net', '20.118.48.12']
 
 
 # Application definition
@@ -146,7 +146,7 @@ EMAIL_USE_TLS = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-CSRF_TRUSTED_ORIGINS = ['https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['127.0.0.1', 'https://m3da1.azurewebsites.net', '20.118.48.12']
 
 # microsoft authentication credentials
 MICROSOFT = {
