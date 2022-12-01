956f6bd8270575c92b662a766db0f0c4382eac63
Ivan Kim Anoos <55166879+ivananoosGithub@users.noreply.github.com>
2022-12-01 17:03:07 +0800

    final update for deployment

--- team_m3da1_project/team_m3da1_project/settings.py
+++ team_m3da1_project/team_m3da1_project/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # note: specify static directory location
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR/'static']
+STATIC_ROOT = BASE_DIR/ "staticfiles"
 STATICFILE_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
